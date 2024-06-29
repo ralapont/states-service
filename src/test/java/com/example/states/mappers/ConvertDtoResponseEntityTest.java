47554adf966ab6feafac120b4c2c017cf7047760
Rafael Alapont Gómez <rafael.alapont.gomez@gmail.com>
2024-06-29 10:41:00 +0200

    problem convert

--- src/test/java/com/example/states/mappers/ConvertDtoResponseEntityTest.java
+++ src/test/java/com/example/states/mappers/ConvertDtoResponseEntityTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-//@AllArgsConstructor
 public class ConvertDtoResponseEntityTest {
 
     @Autowired
